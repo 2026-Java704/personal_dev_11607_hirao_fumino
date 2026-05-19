f9f03977e1dd68daf15be5034af1ff09190bab98
student <student@FJ-1025>
2026-05-19 11:51:15 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/PersonalDev11607HiraoFuminoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11607HiraoFuminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11607HiraoFuminoApplication.class, args);
+	}
+
+}
